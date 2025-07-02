180e1bd314ca0c4e489a63c42cee2e4fbe2d5f7b
RasSilasMotse <ras@rassilasmotse.co.za>
2025-07-02 11:38:27 +0100

    This is my last mini project using python

---
+++ calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #function to calculate the area
+    return length * width
+
+def perimeter(length, width):
+    #fuction to calculate the perimeter
+    return 2* (length + width)
